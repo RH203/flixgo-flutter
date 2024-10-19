0189d52dca23f3526ba3ab65216b101687f348a5
raihanfirdaus <r1aihan2003f1irdaus@gmail.com>
2024-10-19 15:11:32 +0700

    add router, logger, bloc observer, and some packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:bloc/bloc.dart';
+import 'package:flixgo/app_observer.dart';
 import 'package:flixgo/core/injection/injection.dart';
 import 'package:flixgo/src/app/app.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = const AppBlocObserver();
   await initDependencies();
   runApp(
     const App(),
